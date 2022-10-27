19565b376aa5886ced561fbcb45bcf478ec1d359
Carina Tam <carinatam6@gmail.com>
2022-10-27 17:06:15 -0400

    Setup spring boot application

---
+++ src/main/java/ca/proj/seg4105/Seg4105Application.java
@@ -0,0 +1,13 @@
+package ca.proj.seg4105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seg4105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seg4105Application.class, args);
+	}
+
+}
---
+++ src/test/java/ca/proj/seg4105/Seg4105ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.proj.seg4105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seg4105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
