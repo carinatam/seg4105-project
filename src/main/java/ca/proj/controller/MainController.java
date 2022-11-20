9905ab97e498c93ed9cb99d2624cba128c6cfd85
Carina Tam <carinatam6@gmail.com>
2022-11-20 15:56:21 -0500

    Add prettier

--- src/main/java/ca/proj/controller/MainController.java
+++ src/main/java/ca/proj/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class MainController {
-  
+
   @GetMapping("/")
   public String getAdminHomePage() {
     return Page.ADMIN_HOME.getUrl();
