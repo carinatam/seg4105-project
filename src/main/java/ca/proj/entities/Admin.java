0a0dce4d8f2b717c71f16c1f22f4a54ca7f3f2a9
Jodi <dqiao100@uottawa.ca>
2022-11-21 20:33:17 -0500

    mysql config changes and some domain modules

---
+++ src/main/java/ca/proj/entities/Admin.java
@@ -0,0 +1,13 @@
+// package org.springframework.proj.entities;
+
+// /**
+//  * Simple JavaBean domain object with an id property. 
+//  * Used as a base class for objects needing this property.
+//  *
+//  * @author Jodi Qiao
+//  */
+
+
+// public class Admin {
+   
+// }
